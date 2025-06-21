25253e1373a3d5666c62d896fe740be5f8453fd5
Valentyn Radchenko <valentyn.radchenko@outlook.com>
2025-06-21 11:04:04 +0200

    Urls changed for deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8080";
+const API_BASE = "https://inclusivewayserver.onrender.com";
 
 export async function authCallback(data) {
   return request("/auth", { method: "POST", body: data });
